f5d339a2e10e6529a1e5c8e40b98bf4a3301c725
Alex Melville <alex@bitgo.com>
2016-08-15 18:24:48 -0700

    merge with upstream master

--- ethereumj-core/src/main/java/org/ethereum/samples/PrivateMinerSample.java
+++ ethereumj-core/src/main/java/org/ethereum/samples/PrivateMinerSample.java
@@ -3,7 +3,6 @@ package org.ethereum.samples;
 import com.typesafe.config.ConfigFactory;
 import org.ethereum.config.SystemProperties;
 import org.ethereum.core.Block;
-import org.ethereum.core.Transaction;
 import org.ethereum.crypto.ECKey;
 import org.ethereum.facade.EthereumFactory;
 import org.ethereum.mine.Ethash;
